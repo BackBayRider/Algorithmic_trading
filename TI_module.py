6ab10539e6a821313e9ddd36d7c0c3b117435691
CXXIII <charles.teuf@gmail.com>
2017-08-04 16:25:35 +0200

    Update TI_module.py

--- TI_module.py
+++ TI_module.py
@@ -1,1 +1,20 @@
-zezef
+def TI_price(type, S, K, P, Ch):
+    if type == "call":
+        price = (S - K)/(P * Ch)
+    elif type == "put":
+        price = (K - S)/(P * Ch)
+    return price
+
+def TI_strike(type, S, price, P, Ch):
+    if type == "call":
+        K = -1 * price * (P * Ch) + S
+    elif type == "put":
+        K = price * (P * Ch) + S
+    return K
+
+def TI_underlying_price(type, K, price, P, Ch):
+    if type == "call":
+        S = K + price * (P * Ch)
+    elif type == "put":
+        S = K - price * (P * Ch)
+    return S
