385a76fd1d1f17a01154e76e79a6623bb64aacf8
CXXIII <charles.teuf@gmail.com>
2017-08-05 21:22:19 +0200

    adosc_func.py

---
+++ adosc_func.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#Incertitude quant à la façon de calculer l'indicateur. Impossible de retrouver la même série. A voir.
+#Du coup 3 façon de l'implémenter:
+#   - avec le package Ta-Lib
+#   - avec la formule fournie par ChartSchool
+#   - avec la formule fournie par la notice technique Bloomberg
+
+def adosc_talib():
+  
