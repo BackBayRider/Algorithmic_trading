204416e3552106b37b93ede9d675f4d0ab83b800
CXXIII <charles.teuf@gmail.com>
2017-08-05 21:29:29 +0200

    Update adosc_func.py

--- adosc_func.py
+++ adosc_func.py
@@ -1,10 +1,17 @@
 import numpy as np
+from talib import ADOSC
 
 #Incertitude quant à la façon de calculer l'indicateur. Impossible de retrouver la même série. A voir.
 #Du coup 3 façon de l'implémenter:
-#   - avec le package Ta-Lib
-#   - avec la formule fournie par ChartSchool
-#   - avec la formule fournie par la notice technique Bloomberg
+#   - avec le package Ta-Lib.
+#   - avec la formule fournie par ChartSchool (avec EMA sur ADL).
+#   - avec la formule fournie par la notice technique Bloomberg (avec SMA sur ADL).
+#Les functions suivantes admettent des np.array (donc .values sur colonne de dataframe).
 
-def adosc_talib():
+def adosc_talib(my_close, my_high, my_low, my_volume, fastperiod, slowperiod):
+  x = ADOSC(high_price, low_price, close_price, volume, fastperiod = fastoeriod, slowperiod = slowperiod)
+  return x
+
+def adosc_chartschool(my_close, my_high, my_low, my_volume, fastperiod, slowperiod):
   
+  return x
